8ce1aaef18b1a03f7cd0b0778b8c817709b8c7b8
zjj <804530103@qq.com>
2022-11-27 21:23:20 +0800

    Tuple

---
+++ lab1/src/main/java/simpledb/storage/Tuple.java
@@ -0,0 +1,112 @@
+package simpledb.storage;
+
+import simpledb.common.Type;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.Iterator;
+
+/**
+ * Tuple maintains information about the contents of a tuple. Tuples have a
+ * specified schema specified by a TupleDesc object and contain Field objects
+ * with the data for each field.
+ */
+public class Tuple implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    private TupleDesc tupleDesc;
+    private RecordId recordId;
+    private final Field[] fields;
+
+
+    /**
+     * Create a new tuple with the specified schema (type).
+     *
+     * @param td the schema of this tuple. It must be a valid TupleDesc
+     *           instance with at least one field.
+     */
+    public Tuple(TupleDesc td) {
+        tupleDesc = td;
+        fields = new Field[td.numFields()];
+    }
+
+    /**
+     * @return The TupleDesc representing the schema of this tuple.
+     */
+    public TupleDesc getTupleDesc() {
+        // TODO: some code goes here
+        return tupleDesc;
+    }
+
+    /**
+     * @return The RecordId representing the location of this tuple on disk. May
+     *         be null.
+     */
+    public RecordId getRecordId() {
+        return recordId;
+    }
+
+    /**
+     * Set the RecordId information for this tuple.
+     *
+     * @param rid the new RecordId for this tuple.
+     */
+    public void setRecordId(RecordId rid) {
+        this.recordId = rid;
+    }
+
+    /**
+     * Change the value of the ith field of this tuple.
+     *
+     * @param i index of the field to change. It must be a valid index.
+     * @param f new value for the field.
+     */
+    public void setField(int i, Field f) {
+        if(i < 0 || i > fields.length-1){
+            throw new IndexOutOfBoundsException("Index: " + i + "is not a valid index");
+        }
+        fields[i] = f;
+    }
+
+    /**
+     * @param i field index to return. Must be a valid index.
+     * @return the value of the ith field, or null if it has not been set.
+     */
+    public Field getField(int i) {
+        if(i < 0 || i > fields.length-1){
+            throw new IndexOutOfBoundsException("Index: " + i + "is not a valid index");
+        }
+        return fields[i];
+    }
+
+    /**
+     * Returns the contents of this Tuple as a string. Note that to pass the
+     * system tests, the format needs to be as follows:
+     * <p>
+     * column1\tcolumn2\tcolumn3\t...\tcolumnN
+     * <p>
+     * where \t is any whitespace (except a newline)
+     */
+    public String toString() {
+        StringBuilder builder = new StringBuilder();
+        for (int i = 0; i < fields.length;i++){
+            builder.append(fields[i]).append("  ");
+        }
+        return builder.toString();
+    }
+
+    /**
+     * @return An iterator which iterates over all the fields of this tuple
+     */
+    public Iterator<Field> fields() {
+        return Arrays.asList(fields).iterator();
+    }
+
+    /**
+     * reset the TupleDesc of this tuple (only affecting the TupleDesc)
+     */
+    public void resetTupleDesc(TupleDesc td) {
+        this.tupleDesc = td;
+    }
+}
